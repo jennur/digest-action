fa49b5591f4edffb6d796fc865c992cdc997df49
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-10-22 17:46:36 +0200

    Test issue logging

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ async function run() {
     const context = github.context;
     const octokit = github.getOctokit(repo_token);
 
-    const { data: issues } = await octokit.rest.issues.listForRepo({
-        ...context.repo,
-        state: 'open',
-        sort: 'created',
-        direction: 'asc',
-        per_page: 100
-    }).then(issues => {
-      core.info(`Testing ${issues} ...`);
-      // issues is an array of all issue objects
+    const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+      ...context.repo,
+      state: 'open',
+      sort: 'created',
+      direction: 'asc',
+      per_page: 100,
     });
+    
+    // iterate through each response
+    for await (const { data: issues } of iterator) {
+      for (const issue of issues) {
+        console.log("Issue #%d: %s", issue.number, issue.title);
+      }
+    }
 
     // const ms = core.getInput('milliseconds');
-
 
     // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     // await wait(parseInt(ms));
