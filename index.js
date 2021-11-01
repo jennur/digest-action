721e230150c49fd360a181feebdc4c73cff23c62
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-11-01 14:19:33 +0100

    Test discord webhook

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 async function run() {
   try {
     const repo_token = core.getInput('repo-token');
     const context = github.context;
     const octokit = github.getOctokit(repo_token);
+    const WEBHOOK_ID = core.getInput('webhook-id') || '';
+    const WEBHOOK_TOKEN = core.getInput('webhook-token') || '';
 
-    const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
-      ...context.repo,
-      state: 'open',
-      sort: 'created',
-      direction: 'asc',
-      per_page: 100,
-    });
-    
-    // iterate through each response
-    for await (const { data: issues } of iterator) {
-      for (const issue of issues) {
-        console.log("Issue #%d: %s", issue.number, issue.title);
-      }
+    if(WEBHOOK_ID === '' || WEBHOOK_TOKEN === '') {
+      console.log("Cannot find webhook values");
     }
+    else {
+      const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
+        ...context.repo,
+        state: 'open',
+        sort: 'created',
+        direction: 'asc',
+        per_page: 100,
+      });
+      
+      let content = '';
+      // iterate through each response
+      for await (const { data: issues } of iterator) {
+        for (const issue of issues) {
+          let issueMsg = `**${issue.title}**\nby ${issue.user.login} - <${issue.html_url}>\n\n`;
+          console.log(issueMsg);
+          content += issueMsg;
+        }
+      }
 
+      axios.post(`https://discord.com/api/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`, { content })
+      .then(() => console.log("Message sent to Discord"))
+      .catch(error => console.log("Error sending message to Discord:", error.message))
+
+    }
     // const ms = core.getInput('milliseconds');
 
     // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
